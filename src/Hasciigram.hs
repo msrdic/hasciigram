4e44321301e1bcf9d129d3c820292669b66792bd
quantumdot <m.s.qubits@gmail.com>
2017-04-07 00:03:23 +0200

    I am tired.

--- src/Hasciigram.hs
+++ src/Hasciigram.hs
@@ -1,19 +1,19 @@
 module Hasciigram where
 
-histogram :: (Show s, Num a, Ord a, Show a) => [(s, a)] -> IO ()
+histogram :: (Show s, Integral a, Ord a, Show a) => [(s, a)] -> IO ()
 histogram bins = do
   let longestName = maximum $ map (length . show . fst) bins
       largestValue = maximum $ map snd bins
   histogram' bins longestName largestValue
 
-histogram' :: (Show s, Num a, Ord a, Show a) => [(s, a)] -> Int -> a -> IO ()
+histogram' :: (Show s, Integral a, Ord a, Show a) => [(s, a)] -> Int -> a -> IO ()
 histogram' bins longestName largestValue = do
   let normalised = normaliseBins bins largestValue
   printBins normalised longestName largestValue
 
 normaliseBins bins v = bins
 
-printBins :: (Show s, Num a, Ord a, Show a) => [(s, a)] -> Int -> a -> IO ()
+printBins :: (Show s, Integral a, Ord a, Show a) => [(s, a)] -> Int -> a -> IO ()
 printBins (b:bins) longestName largestValue  = do
   -- not 80, but 79, because we reserve one for delimiter
   let binValueLength = 79 - longestName
@@ -23,11 +23,11 @@ printBins (b:bins) longestName largestValue  = do
       binName  = name ++ (replicate (longestName - namelen) ' ') ++ "|"
 
   let value    = normaliseValue (snd b) largestValue
-      binValue = show value
+      binValue = replicate value '█'
 
   putStrLn $ binName ++ binValue
   printBins bins longestName largestValue
 printBins [] _ _ = putStrLn ""
 
-normaliseValue :: Num a => a -> a -> a
-normaliseValue n max = n
+normaliseValue :: Integral a => a -> a -> Int
+normaliseValue n max =  fromIntegral n
